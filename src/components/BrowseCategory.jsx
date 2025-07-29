da1ad30449b7ab29e8d1492850c36ab201880d92
Eduardo Aviles <eduardoaviles357@yahoo.com>
2025-07-28 20:26:05 -0700

    refactored code, the categories are just placement at the moment

--- src/components/BrowseCategory.jsx
+++ src/components/BrowseCategory.jsx
@@ -1,10 +1,11 @@
-
+import Categories from './MainCategoriesSection/categories.jsx';
 const BrowseCategory = () => {
+  const categories = ["Nutrients & Concentrates", "Lights", "Containers", "Soil", "Seeds", "Fertilizers", "Tools & Accessories"];
   return (
-    <section className="overflow-hidden pt-17.5 bg-[var(--white)] w-screen absolute left-0">
+    <section className="relative overflow-hidden pt-17.5 bg-[var(--white)] w-full left-0 border-gray-2 rounded-2xl">
       <div className="w-full px-4 pb-16 mx-auto border-b max-w-7xl sm:px-8 xl:px-0 border-gray-3">
         <div className="swiper categories-carousel common-carousel">
-          <div className="flex items-center justify-between mb-16">
+          <div className="flex items-center justify-between mb-16 px-6 md:px-8 lg:px-12 xl:px-14">
             <div>
               <h2 className="text-xl font-semibold xl:text-heading-5 text-dark">
                 Browser By Category
@@ -26,66 +27,11 @@ const BrowseCategory = () => {
 
           <div className="swiper swiper-initialized swiper-horizontal">
             <div className="swiper-wrapper flex flex-column" style={{transform: "translate3d(0px, 0px, 0px)", transitionDuration: "0ms"}}>
-              <div className="swiper-slide swiper-slide-active" style={{width: "213.33px"}}>
-                <a className="flex flex-col items-center group" href="/categories/laptop">
-                  <div className="w-[130px] h-[130px] bg-[var(--sunflower)] rounded-full flex items-center justify-center mb-4">
-                    <img alt="Category" loading="lazy" width="80" height="80" decoding="async"  style={{color: "transparent"}} src="image" />
-                  </div>
-                  <div className="flex justify-center">
-                    <h3 className="inline-block text-base font-medium text-center duration-500 text-dark group-hover:text-blue">Nutrients &amp; Concentrates</h3>
-                  </div>
-                </a>
-              </div>
-              <div className="swiper-slide swiper-slide-next" style={{width: "213.33px"}}>
-                <a className="flex flex-col items-center group" href="/categories/watches">
-                  <div className="w-[130px] h-[130px] bg-[var(--sunflower)]  rounded-full flex items-center justify-center mb-4">
-                    <img alt="Category" loading="lazy" width="80" height="80" decoding="async" style={{color: "transparent"}} src="image" />
-                  </div>
-                  <div className="flex justify-center">
-                    <h3 className="inline-block text-base font-medium text-center duration-500 text-dark group-hover:text-blue">Lights</h3>
-                  </div>
-                </a>
-              </div>
-              <div className="swiper-slide" style={{width: "213.33px"}}>
-                <a className="flex flex-col items-center group" href="/categories/watches">
-                  <div className="w-[130px] h-[130px] bg-[var(--sunflower)]  rounded-full flex items-center justify-center mb-4">
-                    <img alt="Category" loading="lazy" width="80" height="80" decoding="async" style={{color: "transparent"}} src="image" />
-                  </div>
-                  <div className="flex justify-center">
-                    <h3 className="inline-block text-base font-medium text-center duration-500 text-dark group-hover:text-blue">Containers</h3>
-                  </div>
-                </a>
-              </div>
-              <div className="swiper-slide" style={{width: "213.33px"}}>
-                <a className="flex flex-col items-center group" href="/categories/watches">
-                  <div className="w-[130px] h-[130px] bg-[var(--sunflower)]  rounded-full flex items-center justify-center mb-4">
-                    <img alt="Category" loading="lazy" width="80" height="80" decoding="async" style={{color: "transparent"}} src="image" />
-                  </div>
-                  <div className="flex justify-center">
-                    <h3 className="inline-block text-base font-medium text-center duration-500 text-dark group-hover:text-blue">Soil</h3>
-                  </div>
-                </a>
-              </div>
-              <div className="swiper-slide" style={{width: "213.33px"}}>
-                <a className="flex flex-col items-center group" href="/categories/watches">
-                  <div className="w-[130px] h-[130px] bg-[var(--sunflower)]  rounded-full flex items-center justify-center mb-4">
-                    <img alt="Category" loading="lazy" width="80" height="80" decoding="async" style={{color: "transparent"}} src="image" />
-                  </div>
-                  <div className="flex justify-center">
-                    <h3 className="inline-block text-base font-medium text-center duration-500 text-dark group-hover:text-blue">Seeds</h3>
-                  </div>
-                </a>
-              </div>
-              <div className="swiper-slide" style={{width: "213.33px"}}>
-                <a className="flex flex-col items-center group" href="/categories/watches">
-                  <div className="w-[130px] h-[130px] bg-[var(--sunflower)]  rounded-full flex items-center justify-center mb-4">
-                    <img alt="Category" loading="lazy" width="80" height="80" decoding="async" style={{color: "transparent"}} src="image" />
-                  </div>
-                  <div className="flex justify-center">
-                    <h3 className="inline-block text-base font-medium text-center duration-500 text-dark group-hover:text-blue">Organic</h3>
-                  </div>
-                </a>
-              </div>
+              {categories.map(
+                (category, index) => (
+                  <Categories key={index} category={category}/>
+              )
+              )}
             </div>
           </div>
 
