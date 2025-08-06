b59f635994623ba685353834d600534ab04bcd33
Eduardo Aviles <eduardoaviles357@yahoo.com>
2025-08-05 20:04:50 -0700

    removed comments

--- src/components/MainCategoriesSection/Categories.jsx
+++ src/components/MainCategoriesSection/Categories.jsx
@@ -5,7 +5,7 @@ const Categories = ({category}) => {
     <div className="swiper-slide swiper-slide-active" style={{width: "213.33px"}}>
       <a className="flex flex-col items-center group" href="/categories/laptop">
         <div className="w-[130px] h-[130px] bg-[var(--sunflower)] rounded-full flex items-center justify-center mb-4">
-          <img alt={category} loading="lazy" width="80" height="80" decoding="async"  style={{color: "transparent"}} src="image" />
+          <img className="w-[inherit] h-[inherit] rounded-full bg-cover bg-center" alt={category} loading="lazy" width="80" height="80" decoding="async"  style={{color: "transparent"}} src="almonds/BagOfAlmondSeeds.jpg" />
         </div>
         <div className="flex justify-center">
           <h3 className="inline-block text-base font-medium text-center duration-500 text-dark group-hover:text-blue">{category}</h3>
@@ -16,65 +16,3 @@ const Categories = ({category}) => {
 };
 
 export default Categories;
-
-{/* <div className="swiper-slide swiper-slide-active" style={{width: "213.33px"}}>
-  <a className="flex flex-col items-center group" href="/categories/laptop">
-    <div className="w-[130px] h-[130px] bg-[var(--sunflower)] rounded-full flex items-center justify-center mb-4">
-      <img alt="Category" loading="lazy" width="80" height="80" decoding="async"  style={{color: "transparent"}} src="image" />
-    </div>
-    <div className="flex justify-center">
-      <h3 className="inline-block text-base font-medium text-center duration-500 text-dark group-hover:text-blue">Nutrients &amp; Concentrates</h3>
-    </div>
-  </a>
-</div>
-<div className="swiper-slide swiper-slide-next" style={{width: "213.33px"}}>
-  <a className="flex flex-col items-center group" href="/categories/watches">
-    <div className="w-[130px] h-[130px] bg-[var(--sunflower)]  rounded-full flex items-center justify-center mb-4">
-      <img alt="Category" loading="lazy" width="80" height="80" decoding="async" style={{color: "transparent"}} src="image" />
-    </div>
-    <div className="flex justify-center">
-      <h3 className="inline-block text-base font-medium text-center duration-500 text-dark group-hover:text-blue">Lights</h3>
-    </div>
-  </a>
-</div> */}
-
-{/* <div className="swiper-slide" style={{width: "213.33px"}}>
-  <a className="flex flex-col items-center group" href="/categories/watches">
-    <div className="w-[130px] h-[130px] bg-[var(--sunflower)]  rounded-full flex items-center justify-center mb-4">
-      <img alt="Category" loading="lazy" width="80" height="80" decoding="async" style={{color: "transparent"}} src="image" />
-    </div>
-    <div className="flex justify-center">
-      <h3 className="inline-block text-base font-medium text-center duration-500 text-dark group-hover:text-blue">Containers</h3>
-    </div>
-  </a>
-</div>
-<div className="swiper-slide" style={{width: "213.33px"}}>
-  <a className="flex flex-col items-center group" href="/categories/watches">
-    <div className="w-[130px] h-[130px] bg-[var(--sunflower)]  rounded-full flex items-center justify-center mb-4">
-      <img alt="Category" loading="lazy" width="80" height="80" decoding="async" style={{color: "transparent"}} src="image" />
-    </div>
-    <div className="flex justify-center">
-      <h3 className="inline-block text-base font-medium text-center duration-500 text-dark group-hover:text-blue">Soil</h3>
-    </div>
-  </a>
-</div>
-<div className="swiper-slide" style={{width: "213.33px"}}>
-  <a className="flex flex-col items-center group" href="/categories/watches">
-    <div className="w-[130px] h-[130px] bg-[var(--sunflower)]  rounded-full flex items-center justify-center mb-4">
-      <img alt="Category" loading="lazy" width="80" height="80" decoding="async" style={{color: "transparent"}} src="image" />
-    </div>
-    <div className="flex justify-center">
-      <h3 className="inline-block text-base font-medium text-center duration-500 text-dark group-hover:text-blue">Seeds</h3>
-    </div>
-  </a>
-</div>
-<div className="swiper-slide" style={{width: "213.33px"}}>
-  <a className="flex flex-col items-center group" href="/categories/watches">
-    <div className="w-[130px] h-[130px] bg-[var(--sunflower)]  rounded-full flex items-center justify-center mb-4">
-      <img alt="Category" loading="lazy" width="80" height="80" decoding="async" style={{color: "transparent"}} src="image" />
-    </div>
-    <div className="flex justify-center">
-      <h3 className="inline-block text-base font-medium text-center duration-500 text-dark group-hover:text-blue">Organic</h3>
-    </div>
-  </a>
-</div> */}
