2235e50236d0827c99f534c47277007c0d959ff4
Eduardo Aviles <eduardoaviles357@yahoo.com>
2025-08-06 20:32:42 -0700

    updated pages to include component BrowseCategories

--- src/components/Pages/Login.jsx
+++ src/components/Pages/Login.jsx
@@ -1,7 +1,9 @@
+import BrowseCategory from '../BrowseCategory.jsx';
+
 const Login = () => {
 return (
     <section className="pb-20 mt-35 overflow-hidden bg-gray-2">
-      <div className="w-full px-4 mx-auto max-w-7xl sm:px-8 xl:px-0">
+      <div className="w-full px-4 mx-auto max-w-7xl sm:px-8 xl:px-0 mb-30">
         <div className="max-w-[570px] w-full mx-auto rounded-2xl bg-white shadow-1 p-4 sm:p-7.5 xl:p-11">
           <div className="text-center mb-11">
             <h2 className="font-semibold text-xl sm:text-2xl xl:text-heading-5 text-dark mb-1.5">Log In to Your Account</h2>
@@ -22,6 +24,7 @@ return (
           </div>
         </div>
       </div>
+      <BrowseCategory />
     </section>
 )}
 export default Login;
