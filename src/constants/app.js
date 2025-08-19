6e9fae8d6bf2d51e9960a4eb3cee0e245dfa92ce
Eduardo Aviles <eduardoaviles357@yahoo.com>
2025-08-18 21:26:57 -0700

    new folders added, hooks added with useLocalStorage, handling local storage

---
+++ src/constants/app.js
@@ -0,0 +1,1 @@
+export const TOKEN_STORAGE_ID = "ecommerceplants-token";
---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+
+// Custom hook for keeping state data synced with localStorage.
+
+function useLocalStorage(key, firstValue = null) {
+  const initialValue = localStorage.getItem(key) || firstValue;
+
+  const [item, setItem] = useState(initialValue);
+
+  useEffect(function setKeyInLocalStorage() { // rerun effect when key or item change
+    console.debug("HOOKS <<====>> useLocalStorage useEffect >>>> ITEM::", item);
+
+    if (item === null) { // if nothing exist remove key
+      localStorage.removeItem(key);
+    } else { // if exist set key and value to item
+      localStorage.setItem(key, item);
+    }
+  }, [key, item]);
+
+  return [item, setItem]; // return state and setter function
+}
+
+export default useLocalStorage;
