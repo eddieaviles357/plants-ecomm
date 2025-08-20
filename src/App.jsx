1f094d6cd725fbdcbf8a904c48ce9dbe4a719f09
Eduardo Aviles <eduardoaviles357@yahoo.com>
2025-08-19 20:40:03 -0700

    moved code from UserContext to our app component, will have to refactor the code later on in the life cycle of the component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import { TOKEN_STORAGE_ID, PATHS } from './constants/app.js';
 import Navigation from './components/Header/Navigation.jsx';
 import Home from './components/Pages/Home.jsx';
 import Products from './components/Pages/Products.jsx';
@@ -10,25 +11,115 @@ import About from './components/Pages/About.jsx';
 import ProductDetails from './components/Product/ProductDetails.jsx';
 import './App.css'
 
-function App() {
+import UserContext from './components/Auth/UserContext.jsx';
+import ECommercePlantsAppAPI from './api/ECommercePlantsAppAPI.js';
 
+function App() {
+  const [infoLoaded, setInfoLoaded] = useState(false);
+  const [currentUser, setCurrentUser] = useState(null);
+  const [token, setToken] = useLocalStorage(TOKEN_STORAGE_ID);
+  const [errors, setErrors] = useState(null);
+  
+  useEffect(() => {
+    console.debug("App useEffect LOAD_USER_INFO\n", "token=", token);
+
+    async function getCurrentUser() {
+      if (token) {
+        try {
+          let { username } = jwt.decode(token);
+          // add token to api class so we can interact with
+          ECommercePlantsAppAPI.token = token;
+          let currentUser = await ECommercePlantsAppAPI.getCurrentUser(username);
+          setCurrentUser(currentUser);
+        } catch (err) {
+          console.error("App LOAD_USER_INFO: problem_loading", err);
+          setCurrentUser(null);
+        }
+      };
+      setInfoLoaded(true);
+    }
+    setInfoLoaded(false);
+    getCurrentUser();
+  }, [token]);
+
+  useEffect(() => {
+    async function fetchRecents() {
+      if(token && currentUser) {
+        try {
+          const {username} = currentUser;
+          const {recents} = await ECommercePlantsAppAPI.getRecents( username ) || [];
+          setVisitedNews( JSON.stringify(recents) );
+        } catch (err) {
+          setErrors(Array.from(err || err.message));
+          console.log(err);
+        };
+      }
+    };
+    fetchRecents();
+  }, [currentUser]);
+
+  // Handles site logout.
+  async function logout() {
+    const { username } = currentUser;
+    console.log("__LOGGIING_OUT__", username);
+    setCurrentUser(null);
+    setToken(null);
+    setVisitedNews(null);
+  }
+
+  /** Handles site SIGNUP.
+ *
+ * Automatically logs them in (set token) upon SIGNUP.
+ *
+ */
+  async function signup(signupData) {
+    try {
+      let token = await ECommercePlantsAppAPI.signup(signupData);
+      setToken(token);
+      return { success: true };
+    } catch (errors) {
+      console.error("signup_failed", errors);
+      return { success: false, errors };
+    }
+  };
+
+  // Handles site LOGIN.
+
+  async function login(loginData) {
+    try {
+      let token = await ECommercePlantsAppAPI.login(loginData);
+      setToken(token);
+      return { success: true };
+    } catch (errors) {
+      console.error("login failed", errors);
+      return { success: false, errors: errors };
+    }
+  };
+
+  if (!infoLoaded) return <div className="text-center">Loading...</div>;
+  
   return (
-    <>
+    <UserContext.Provider value={{ 
+        currentUser, 
+        setCurrentUser,
+        errors,
+        setErrors,
+        }}>
       <Navigation />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/products/:id" element={<ProductDetails />} />
-        <Route path="*" element={<h1>404 Not Found</h1>} />
+        <Route path={PATHS.HOME} element={<Home />} />
+        <Route path={PATHS.PRODUCTS} element={<Products />} />
+        <Route path={PATHS.LOGIN} element={<Login />} />
+        <Route path={PATHS.SIGNUP} element={<Signup />} />
+        <Route path={PATHS.ABOUT}element={<About />} />
+        <Route path={PATHS.PRODUCT_DETAILS} element={<ProductDetails />} />
+        <Route path={PATHS.NOT_FOUND} element={<h1>404 Not Found</h1>} />
       </Routes>
       <main>
         {/* <BackToTopButton /> */}
         <Footer />
       </main>
-    </>
+    </UserContext.Provider>
   )
 }
 
