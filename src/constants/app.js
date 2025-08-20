f80ba81da2d0f9ad190c6956ee215bf1d2af57ed
Eduardo Aviles <eduardoaviles357@yahoo.com>
2025-08-19 20:41:09 -0700

    PATH constants added, we use Objet.freeze to make the object immutable

--- src/constants/app.js
+++ src/constants/app.js
@@ -1,1 +1,12 @@
 export const TOKEN_STORAGE_ID = "ecommerceplants-token";
+
+export const PATHS = Object.freeze({
+  HOME: "/",
+  PRODUCTS: "/products",
+  PRODUCT_DETAILS: "/products/:id",
+  CART: "/cart",
+  LOGIN: "/login",
+  SIGNUP: "/signup",
+  ABOUT: "/about",
+  NOT_FOUND: "*",
+})
