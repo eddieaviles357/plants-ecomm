9d860e7e34d12f48ff6f047c87970dfef795360a
Eduardo Aviles <eduardoaviles357@yahoo.com>
2025-08-22 19:39:09 -0700

    private route (empty right now), RoutesApp component added

---
+++ src/components/RoutesApp/PrivateRoute.jsx
@@ -0,0 +1,26 @@
+import React, { useContext, useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import UserContext from "../Auth/UserContext";
+
+/** "Higher-Order Component" for private routes.
+ *
+ * In routing component. 
+ * This component will check if isAllowed is true
+ * and only continues to the route if so. 
+ * If isAllowed is true, redirects using redirectPath prop.
+ * Default redirect is login form.
+ */
+
+function PrivateRoute({ redirectPath = "/login", children }) {
+  const { currentUser } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  // redirect no current user exist
+  useEffect(() => {
+    if(!currentUser) navigate(redirectPath)
+  }, [currentUser, navigate, redirectPath])
+  // can be used as layout component or a wrapper component
+  return children ? children : <Outlet />;
+}
+
+export default PrivateRoute;
---
+++ src/components/RoutesApp/RoutesApp.jsx
@@ -0,0 +1,27 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from '../Pages/Home.jsx';
+import Products from '../Pages/Products.jsx';
+import Login from '../Pages/Login.jsx';
+import Signup from '../Pages/Signup.jsx';
+import About from '../Pages/About.jsx';
+import ProductDetails from '../Product/ProductDetails.jsx';
+import PrivateRoute from './PrivateRoute.jsx';
+import { PATHS } from '../../constants/app.js';
+const RoutesApp = ({ login, signup }) => {
+  return (
+    <Routes>
+      <Route path={PATHS.HOME} element={<Home />} />
+      <Route path={PATHS.PRODUCTS} element={<Products />} />
+      <Route path={PATHS.LOGIN} element={<Login login={login}/>} />
+      <Route path={PATHS.SIGNUP} element={<Signup signup={signup}/>} />
+      <Route path={PATHS.ABOUT}element={<About />} />
+      <Route path={PATHS.PRODUCT_DETAILS} element={<ProductDetails />} />
+      <Route path={PATHS.NOT_FOUND} element={<h1>404 Not Found</h1>} />
+      <Route element={<PrivateRoute />}>
+        {/* <Route path="/profile" element={<Profile />} /> */}
+      </Route>
+    </Routes>
+  )
+}
+
+export default RoutesApp;
