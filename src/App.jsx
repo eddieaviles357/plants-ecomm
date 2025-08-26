c42677d9e2379cf69a1b4794ea0517f3e588ab12
Eduardo Aviles <eduardoaviles357@yahoo.com>
2025-08-25 20:57:26 -0700

    app component updated, removed one useEffect code that was not being used. Implemented jwt tokens when user signs in

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import useLocalStorage from './hooks/useLocalStorage.js';
 // import jwt from 'jsonwebtoken';
-import { useJwt } from 'react-jwt';
+// import { useJwt } from 'react-jwt';
+import { isExpired, decodeToken } from "react-jwt";
 // import { useParams } from 'react-router-dom';
 import ECommercePlantsAppAPI from './api/ECommercePlantsAppAPI.js';
-import { Routes, Route } from 'react-router-dom';
 import { TOKEN_STORAGE_ID, PATHS } from './constants/app.js';
 import UserContext from './components/Auth/UserContext.jsx';
 import Navigation from './components/Header/Navigation.jsx';
-import Home from './components/Pages/Home.jsx';
-import Products from './components/Pages/Products.jsx';
 import BackToTopButton from './components/BackToTopButton.jsx';
 import Footer from './components/Footer/Footer.jsx';
-import Login from './components/Pages/Login.jsx';
-import Signup from './components/Pages/Signup.jsx';
-import About from './components/Pages/About.jsx';
-import ProductDetails from './components/Product/ProductDetails.jsx';
+import RoutesApp from './components/RoutesApp/RoutesApp.jsx';
 import './App.css'
 
 
@@ -24,22 +19,29 @@ function App() {
   const [currentUser, setCurrentUser] = useState(null);
   const [token, setToken] = useLocalStorage(TOKEN_STORAGE_ID);
   const [errors, setErrors] = useState(null);
-  const { decodeToken, isExpired } = useJwt(token);
+  // const { isExpired, decodeToken } = useJwt(token);
+  const decodedToken = decodeToken(token);
+  const isTokenExpired = isExpired(token);
   
   useEffect(() => {
     console.debug("App useEffect LOAD_USER_INFO\n", "token=", token);
-
+    
     async function getCurrentUser() {
       if (token) {
         try {
-          // let { username } = jwt.decode(token);
-          let { username } = decodeToken(token);
+          let userData = decodeToken(token);
+          let {username} = userData
+          console.debug("DECODED_TOKEN", decodedToken);
+          console.debug("IS_TOKEN_EXPIRED", isTokenExpired);
+          console.debug("userData=", userData);
           // add token to api class so we can interact with
           ECommercePlantsAppAPI.token = token;
-          let currentUser = await ECommercePlantsAppAPI.getCurrentUser(username);
+          let currentUser = username || null;
+          console.debug("App LOAD_USER_INFO: currentUser=", currentUser);
           setCurrentUser(currentUser);
         } catch (err) {
           console.error("App LOAD_USER_INFO: problem_loading", err);
+          setErrors(Array.from(err || err.message));
           setCurrentUser(null);
         }
       };
@@ -49,29 +51,12 @@ function App() {
     getCurrentUser();
   }, [token]);
 
-  useEffect(() => {
-    async function fetchRecents() {
-      if(token && currentUser) {
-        try {
-          const {username} = currentUser;
-          const {recents} = await ECommercePlantsAppAPI.getRecents( username ) || [];
-          setVisitedNews( JSON.stringify(recents) );
-        } catch (err) {
-          setErrors(Array.from(err || err.message));
-          console.log(err);
-        };
-      }
-    };
-    fetchRecents();
-  }, [currentUser]);
-
   // Handles site logout.
   async function logout() {
     const { username } = currentUser;
     console.log("__LOGGIING_OUT__", username);
     setCurrentUser(null);
     setToken(null);
-    setVisitedNews(null);
   }
 
   /** Handles site SIGNUP.
@@ -96,6 +81,7 @@ function App() {
     try {
       let token = await ECommercePlantsAppAPI.login(loginData);
       setToken(token);
+      console.debug("login successful! token:", token);
       return { success: true };
     } catch (errors) {
       console.error("login failed", errors);
@@ -111,17 +97,12 @@ function App() {
         setCurrentUser,
         errors,
         setErrors,
+        signup,
+        login,
+        logout
         }}>
-      <Navigation />
-      <Routes>
-        <Route path={PATHS.HOME} element={<Home />} />
-        <Route path={PATHS.PRODUCTS} element={<Products />} />
-        <Route path={PATHS.LOGIN} element={<Login />} />
-        <Route path={PATHS.SIGNUP} element={<Signup />} />
-        <Route path={PATHS.ABOUT}element={<About />} />
-        <Route path={PATHS.PRODUCT_DETAILS} element={<ProductDetails />} />
-        <Route path={PATHS.NOT_FOUND} element={<h1>404 Not Found</h1>} />
-      </Routes>
+      <Navigation logout={logout}/>
+      <RoutesApp logout={logout} signup={signup}/>
       <main>
         {/* <BackToTopButton /> */}
         <Footer />
