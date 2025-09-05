0dc002eec6f280a70848cd2838fde5d69161cc9c
Eduardo Aviles <eduardoaviles357@yahoo.com>
2025-09-04 19:50:38 -0700

    userContext import for user related functionality (child props basically)

--- src/components/Auth/SignupForm.jsx
+++ src/components/Auth/SignupForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import UserContext from "./UserContext";
+import UserContext from "../Context/UserContext";
 
 
 const SignupForm = ({ signup }) => {
