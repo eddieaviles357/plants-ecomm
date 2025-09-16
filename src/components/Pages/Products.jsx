655c73a5f252fc358e2e3fabd937c452f80220bd
Eduardo Aviles <eduardoaviles357@yahoo.com>
2025-09-15 20:48:04 -0700

    import context ProductCategoriesContext, updated componenet name

--- src/components/Pages/Products.jsx
+++ src/components/Pages/Products.jsx
@@ -1,22 +1,27 @@
 import { useState, useContext } from 'react';
-import ProductsPageCategories from '../Categories/ProductsPageCategories';
+import ProductsPageCategoriesSelection from '../Categories/ProductsPageCategoriesSelection';
 import ProductCard from '../Product/ProductCard';
 import ProductsContext from '../Context/ProductsContext.jsx';
+import ProductCategoriesContext from '../Context/ProductCategoriesContext.jsx';
 
 const Products = () => {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const { products, setProducts } = useContext(ProductsContext);
+    const { productCategories, setProductCategories } = useContext(ProductCategoriesContext);
 
+    // console.log("productCategories length", productCategories);
+    // console.log("Products page products", products);
+    // console.log("Products page productCategories", productCategories);
     return (
       <section className="relative pt-35 overflow-hidden bg-gray-2">
         <div className="w-full px-4 mx-auto max-w-7xl sm:px-8 xl:px-0">
           <div className="grid gap-6 xl:grid-cols-12">
             <div className="sidebar-content fixed xl:static bg-gray-2 xl:bg-transparent xl:translate-x-0 xl:rounded-xl left-0 top-0 xl:col-span-3 w-[290px] 
                             sm:w-[320px] xl:w-full h-full xl:h-auto  z-99 xl:z-auto transition-transform duration-300 ease-in-out -translate-x-full">
-              <ProductsPageCategories />
+              <ProductsPageCategoriesSelection />
             </div>
-            <ProductCard products={products} />
+            {(productCategories.length > 0) ? (<ProductCard products={productCategories} />) :  (<ProductCard products={products} />)}
           </div>
         </div>
       </section>
