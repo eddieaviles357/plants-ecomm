f3baa5f9b2b2fe2f0649b868974aac1f0e502e86
Eduardo Aviles <eduardoaviles357@yahoo.com>
2025-09-29 20:54:32 -0700

    Link import replaced anchor tag to avoid full page load

--- src/components/MainCategoriesSection/CategoriesList.jsx
+++ src/components/MainCategoriesSection/CategoriesList.jsx
@@ -1,5 +1,6 @@
 import CategoriesContext from '../Context/CategoriesContext.jsx';
 import { useContext } from 'react';
+import { Link } from 'react-router-dom';
 
 const CategoriesList = () => {
   const {categories} = useContext(CategoriesContext);
@@ -8,14 +9,14 @@ const CategoriesList = () => {
     <div className="swiper-slide swiper-slide-active flex flex-row" style={{width: "213.33px"}}>
       {categories.map(
         ({category, id}) => (
-          <a key={id} className="flex flex-col items-center group" href={`/categories/${category}`}>
+          <Link to={`/categories/${id}`} key={id} className="flex flex-col items-center group">
             <div className="w-[130px] h-[130px] bg-[var(--sunflower)] rounded-full flex items-center justify-center mb-4">
               <img className="w-[inherit] h-[inherit] rounded-full bg-cover bg-center" alt={category} loading="lazy" width="80" height="80" decoding="async"  style={{color: "transparent"}} src={`almonds/BagOfAlmondSeeds.jpg`} />
             </div>
             <div className="flex justify-center">
               <h3 className="inline-block text-base font-medium text-center duration-500 text-dark group-hover:text-blue">{category}</h3>
             </div>
-          </a>
+          </Link>
         )
       )}
     </div>
